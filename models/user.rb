8997a85862e18d12fbfab2745da96ee4ce63030d
Guillermo Iguaran <guilleiguaran@gmail.com>
2014-03-29 01:43:38 -0500

    Add associations for User

--- models/user.rb
+++ models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
   has_many :tenshis, :dependent => :destroy
-  has_many :followers
-  has_many :followings
+  has_many :follower_relationships, :class_name => "Relationship", :foreign_key => :following_id
+  has_many :following_relationships, :class_name => "Relationship", :foreign_key => :follower_id
+  has_many :followers, :through => :follower_relationships
+  has_many :followings, :through => :following_relationships
 end
