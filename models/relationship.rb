8668583f3f4902f1891d810185a5c4bec93ce351
angela4713 <ang3l_gu@hotmail.com>
2014-03-29 00:36:13 -0500

    add associations

--- models/relationship.rb
+++ models/relationship.rb
@@ -1,3 +1,4 @@
 class Relationship < ActiveRecord::Base
-
+  belongs_to :follower, class_name: "User"
+  belongs_to :following, class_name: "User"
 end
--- models/user.rb
+++ models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
   has_many :tenshis, :dependent => :destroy
+  has_many :followers
+  has_many :followings
 end
